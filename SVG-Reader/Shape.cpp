9a2e0842cdca427cada632183373b98b5047a691
anvn12 <anmega83@gmail.com>
2025-06-08 17:03:56 +0700

    fix

--- SVG-Reader/Shape.cpp
+++ SVG-Reader/Shape.cpp
@@ -3,14 +3,25 @@
 
 VOID SVGRectangle::processAttribute(char* attributeName, char* attributeValue)
 {
-	if (attributeName == "fill-opacity") {
-		fillOpacity = atoi(attributeValue);
+	if (strcmp(attributeName, "fill-opacity") == 0) {
+		fillOpacity = atof(attributeValue);
 	}
-	else if (attributeName == "x") {
-		position.x = atoi(attributeValue);
+	else if (strcmp(attributeName, "x") == 0) {
+		position.x = atof(attributeValue);
 	}
-	else if (attributeName == "y") {
-		position.y = atoi(attributeValue);
+	else if (strcmp(attributeName, "y") == 0) {
+		position.y = atof(attributeValue);
 	}
+	else if (strcmp(attributeName, "width") == 0) {
+		width = atoi(attributeValue);
+	}
+	else if (strcmp(attributeName, "height") == 0) {
+		height = atoi(attributeValue);
+	}
+	else if (strcmp(attributeName, "stroke-width") == 0) {
+		strokeWidth = atof(attributeValue);
+	}
+	//if (strcmp(attributeName, "stroke") == 0) stroke = attributeValue;
+	//if (strcmp(attributeName, "fill") == 0) fill = value;
 }
 
--- SVG-Reader/Shape.h
+++ SVG-Reader/Shape.h
@@ -28,4 +28,108 @@ public:
 	VOID draw(Graphics &graphic);
 };
 
+/*class Rectangle{
+	int x, y, width, height;
+	string stroke, fill;
+	float fillOpacity, strokeWidth;
+
+	
+
+};*/
+
+class Text {
+	int x, y;
+	string fill;
+	float fontSize;
+
+	void processAttribute(const char* name, const char* value) {
+		if (strcmp(name, "x") == 0) x = atoi(value);
+		if (strcmp(name, "y") == 0) y = atoi(value);
+		if (strcmp(name, "fill") == 0) fill = value;
+		if (strcmp(name, "font-size") == 0) fontSize = atof(value);
+	}
+};
+
+class Circle {
+	int cx, cy, r;
+	string fill, stroke;
+	float strokeWidth, strokeOpacity, fillOpacity;
+
+	void processAttribute(const char* name, const char* value) {
+		if (strcmp(name, "cx") == 0) cx = atoi(value);
+		if (strcmp(name, "cy") == 0) cy = atoi(value);
+		if (strcmp(name, "r") == 0) r = atoi(value);
+		if (strcmp(name, "fill") == 0) fill = value;
+		if (strcmp(name, "stroke") == 0) stroke = value;
+		if (strcmp(name, "stroke-width") == 0) strokeWidth = atof(value);
+		if (strcmp(name, "stroke-opacity") == 0) strokeOpacity = atof(value);
+		if (strcmp(name, "fill-opacity") == 0) fillOpacity = atoi(value);
+	}
+};
+
+class Polyline {
+	string fill, stroke, points;
+	float strokeWidth, strokeOpacity, fillOpacity;
+
+	void processAttribute(const char* name, const char* value) {
+		if (strcmp(name, "fill") == 0) fill = value;
+		if (strcmp(name, "stroke") == 0) stroke = value;
+		if (strcmp(name, "stroke-width") == 0) strokeWidth = atof(value);
+		if (strcmp(name, "stroke-opacity") == 0) strokeOpacity = atof(value);
+		if (strcmp(name, "fill-opacity") == 0) fillOpacity = atoi(value);
+		if (strcmp(name, "points") == 0) points = value;
+	}
+};
+
+class Ellipse {
+	int cx, cy, rx, ry;
+	string fill, stroke;
+	float strokeWidth, strokeOpacity, fillOpacity;
+
+	void processAttribute(const char* name, const char* value) {
+		if (strcmp(name, "cx") == 0) cx = atoi(value);
+		if (strcmp(name, "cy") == 0) cy = atoi(value);
+		if (strcmp(name, "rx") == 0) rx = atoi(value);
+		if (strcmp(name, "ry") == 0) ry = atoi(value);
+		if (strcmp(name, "fill") == 0) fill = value;
+		if (strcmp(name, "stroke") == 0) stroke = value;
+		if (strcmp(name, "stroke-width") == 0) strokeWidth = atof(value);
+		if (strcmp(name, "stroke-opacity") == 0) strokeOpacity = atof(value);
+		if (strcmp(name, "fill-opacity") == 0) fillOpacity = atoi(value);
+	}
+};
+
+class Line {
+	int x1, x2, y1, y2;
+	string stroke;
+	float strokeOpacity, strokeWidth;
+
+	void processAttribute(const char* name, const char* value) {
+		if (strcmp(name, "x1") == 0) x1 = atoi(value);
+		if (strcmp(name, "x2") == 0) x2 = atoi(value);
+		if (strcmp(name, "y1") == 0) y1 = atoi(value);
+		if (strcmp(name, "y2") == 0) y2 = atoi(value);
+		if (strcmp(name, "stroke") == 0) stroke = value;;
+		if (strcmp(name, "stroke-opacity") == 0) strokeOpacity = atof(value);
+		if (strcmp(name, "stroke-width") == 0) strokeWidth = atof(value);
+	}
+};
+
+class Polygon {
+	string fill, stroke, points;
+	float strokeWidth, strokeOpacity, fillOpacity;
+
+	void processAttribute(const char* name, const char* value) {
+		if (strcmp(name, "fill") == 0) fill = value;
+		if (strcmp(name, "stroke") == 0) stroke = value;
+		if (strcmp(name, "stroke-width") == 0) strokeWidth = atof(value);
+		if (strcmp(name, "stroke-opacity") == 0) strokeOpacity = atof(value);
+		if (strcmp(name, "fill-opacity") == 0) fillOpacity = atoi(value);
+		if (strcmp(name, "points") == 0) points = value;
+	}
+};
+
+
+
+
 #endif
