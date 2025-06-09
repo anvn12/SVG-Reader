78920648469afa1751cd465868c469cd61c20399
nhnam <nguyenhainam10009000@gmail.com>
2025-06-09 22:00:00 +0700

    4 encap

--- SVG-Reader/Shape.h
+++ SVG-Reader/Shape.h
@@ -6,8 +6,7 @@
 #include <sstream>
 
 class SVGRectangle {
-//private:
-public:
+private:
 	double fillOpacity;
 	RGBColor stroke;
 	RGBColor fill;
@@ -16,6 +15,7 @@ public:
 	int width;
 	int height;
 
+public:
 	SVGRectangle() {
 		fillOpacity = 0;
 		stroke = RGBColor();
@@ -29,36 +29,78 @@ public:
 	VOID draw(Graphics &graphics);
 };
 
-class Text {
-	int x, y;
-	string fill;
-	float fontSize;
+//class Text {
+//	int x, y;
+//	string fill;
+//	float fontSize;
+//
+//	void processAttribute(const char* name, const char* value) {
+//		if (strcmp(name, "x") == 0) x = atoi(value);
+//		if (strcmp(name, "y") == 0) y = atoi(value);
+//		if (strcmp(name, "fill") == 0) fill = value;
+//		if (strcmp(name, "font-size") == 0) fontSize = atof(value);
+//	}
+//};
 
-	void processAttribute(const char* name, const char* value) {
-		if (strcmp(name, "x") == 0) x = atoi(value);
-		if (strcmp(name, "y") == 0) y = atoi(value);
-		if (strcmp(name, "fill") == 0) fill = value;
-		if (strcmp(name, "font-size") == 0) fontSize = atof(value);
+class SVGText {
+private:
+	Point2D position;
+	RGBColor fill;
+	double fontSize;
+	string content;
+
+public:
+	SVGText() {
+		position = Point2D();
+		fill = RGBColor();
+		fontSize = 0;
 	}
+	VOID processAttribute(char* attributeName, char* attributeValue);
+	VOID setContent(char* attributeValue);
+
+	VOID draw(Graphics& graphics);
 };
 
-class Circle {
-	int cx, cy, r;
-	string fill, stroke;
-	float strokeWidth, strokeOpacity, fillOpacity;
+class SVGCircle {
+private:
+	Point2D position;
+	float r;
+	RGBColor fill, stroke;
+	double strokeWidth, strokeOpacity, fillOpacity;
 
-	void processAttribute(const char* name, const char* value) {
-		if (strcmp(name, "cx") == 0) cx = atoi(value);
-		if (strcmp(name, "cy") == 0) cy = atoi(value);
-		if (strcmp(name, "r") == 0) r = atoi(value);
-		if (strcmp(name, "fill") == 0) fill = value;
-		if (strcmp(name, "stroke") == 0) stroke = value;
-		if (strcmp(name, "stroke-width") == 0) strokeWidth = atof(value);
-		if (strcmp(name, "stroke-opacity") == 0) strokeOpacity = atof(value);
-		if (strcmp(name, "fill-opacity") == 0) fillOpacity = atoi(value);
+public:
+	SVGCircle() {
+		position = Point2D();
+		r = 0;
+		fill = RGBColor();
+		stroke = RGBColor();
+		strokeWidth = 0;
+		fillOpacity = 0;
+		strokeOpacity = 0;
 	}
+	VOID processAttribute(char* attributeName, char* attributeValue);
+
+	VOID draw(Graphics& graphics);
 };
 
+//class Circle {
+//	int cx, cy, r;
+//	string fill, stroke;
+//	float strokeWidth, strokeOpacity, fillOpacity;
+//
+//	void processAttribute(const char* name, const char* value) {
+//		if (strcmp(name, "cx") == 0) cx = atoi(value);
+//		if (strcmp(name, "cy") == 0) cy = atoi(value);
+//		if (strcmp(name, "r") == 0) r = atoi(value);
+//		if (strcmp(name, "fill") == 0) fill = value;
+//		if (strcmp(name, "stroke") == 0) stroke = value;
+//		if (strcmp(name, "stroke-width") == 0) strokeWidth = atof(value);
+//		if (strcmp(name, "stroke-opacity") == 0) strokeOpacity = atof(value);
+//		if (strcmp(name, "fill-opacity") == 0) fillOpacity = atoi(value);
+//	}
+//};
+
+//lam polyline di
 class Polyline {
 	string fill, stroke, points;
 	float strokeWidth, strokeOpacity, fillOpacity;
@@ -73,23 +115,46 @@ class Polyline {
 	}
 };
 
-class Ellipse {
-	int cx, cy, rx, ry;
-	string fill, stroke;
-	float strokeWidth, strokeOpacity, fillOpacity;
+class SVGEllipse {
+private:
+	float rx, ry;
+	Point2D position;
+	RGBColor fill, stroke;
+	double strokeWidth, strokeOpacity, fillOpacity;
 
-	void processAttribute(const char* name, const char* value) {
-		if (strcmp(name, "cx") == 0) cx = atoi(value);
-		if (strcmp(name, "cy") == 0) cy = atoi(value);
-		if (strcmp(name, "rx") == 0) rx = atoi(value);
-		if (strcmp(name, "ry") == 0) ry = atoi(value);
-		if (strcmp(name, "fill") == 0) fill = value;
-		if (strcmp(name, "stroke") == 0) stroke = value;
-		if (strcmp(name, "stroke-width") == 0) strokeWidth = atof(value);
-		if (strcmp(name, "stroke-opacity") == 0) strokeOpacity = atof(value);
-		if (strcmp(name, "fill-opacity") == 0) fillOpacity = atoi(value);
+public:
+	SVGEllipse() {
+		rx = 0;
+		ry = 0;
+		position = Point2D();
+		fill = RGBColor();
+		stroke = RGBColor();
+		strokeWidth = 0;
+		strokeOpacity = 0;
+		fillOpacity = 0;
 	}
+	VOID processAttribute(char* attributeName, char* attributeValue);
+
+	VOID draw(Graphics& graphics);
 };
+
+//class Ellipse {
+//	int cx, cy, rx, ry;
+//	string fill, stroke;
+//	float strokeWidth, strokeOpacity, fillOpacity;
+//
+//	void processAttribute(const char* name, const char* value) {
+//		if (strcmp(name, "cx") == 0) cx = atoi(value);
+//		if (strcmp(name, "cy") == 0) cy = atoi(value);
+//		if (strcmp(name, "rx") == 0) rx = atoi(value);
+//		if (strcmp(name, "ry") == 0) ry = atoi(value);
+//		if (strcmp(name, "fill") == 0) fill = value;
+//		if (strcmp(name, "stroke") == 0) stroke = value;
+//		if (strcmp(name, "stroke-width") == 0) strokeWidth = atof(value);
+//		if (strcmp(name, "stroke-opacity") == 0) strokeOpacity = atof(value);
+//		if (strcmp(name, "fill-opacity") == 0) fillOpacity = atoi(value);
+//	}
+//};
 
 class Line {
 	int x1, x2, y1, y2;
