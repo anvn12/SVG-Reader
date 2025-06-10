4c335c78040fa9abe8dd5e455d6528d9cf18891f
anvn12 <anmega83@gmail.com>
2025-06-10 23:27:34 +0700

    singleton pattern

--- SVG-Reader/main.cpp
+++ SVG-Reader/main.cpp
@@ -5,19 +5,15 @@
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow) {
 	// SINGLETON PATTERN
-	//SVGReader& svgreader = SVGReader::getInstance();
 
 	// init GDI+, WNDCLASSEX, HWND,  
 	// and show the window
-	//svgreader.init(hInstance, iCmdShow);
 	SVGReader::getInstance().init(hInstance, iCmdShow);
 
 	// the main loop for processing messages
-	//svgreader.messageLoop();
 	SVGReader::getInstance().messageLoop();
 
 	// shutdown the gdi+
-	//return svgreader.cleanup();
 	return SVGReader::getInstance().cleanup();
 }  // WinMain
 
