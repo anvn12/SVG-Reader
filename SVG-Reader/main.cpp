75c89dfa3015f3891c38927f198863a9f19ccef5
anvn12 <anmega83@gmail.com>
2025-06-23 23:32:25 +0700

    add reason for singleton pattern

--- SVG-Reader/main.cpp
+++ SVG-Reader/main.cpp
@@ -2,7 +2,12 @@
 #include "SVGReader.h"
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow) {
-	// SINGLETON PATTERN
+	// Use Singleton design pattern as this program is designed to 
+	// display 1 image, also it is simple to design. 
+	// 
+	// If user wants to display more than 1, just launch a new app.
+	// 
+	// The concept is similar to a web browser tab (it only displays 1 at a time).
 
 	// init GDI+, WNDCLASSEX, HWND,  
 	// and show the window
