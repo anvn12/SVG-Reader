ba7033de457dfd041d8f462ea60da79cc9fe4277
anvn12 <anmega83@gmail.com>
2025-06-24 00:22:10 +0700

    comment

--- SVG-Reader/ProcessXML.h
+++ SVG-Reader/ProcessXML.h
@@ -1,16 +1,16 @@
-#ifndef ProcessXML_h
-#define ProcessXML_h
-#pragma once
-
-#include "rapidxml.hpp"
-#include "Shape.h"
-#include <fstream>
-#include <vector>
-
-using namespace std;
-using namespace rapidxml;
-
-//void renderShapes(SVGShape* shape, xml_node<>* node, Graphics& graphics);
-//VOID processXML(Graphics& graphics, string filename);
-
-#endif
+//#ifndef ProcessXML_h
+//#define ProcessXML_h
+//#pragma once
+//
+//#include "rapidxml.hpp"
+//#include "Shape.h"
+//#include <fstream>
+//#include <vector>
+//
+//using namespace std;
+//using namespace rapidxml;
+//
+////void renderShapes(SVGShape* shape, xml_node<>* node, Graphics& graphics);
+////VOID processXML(Graphics& graphics, string filename);
+//
+//#endif
