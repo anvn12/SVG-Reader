b95a73427c703bab5b3afb72cd5d6914e2d93087
anvn12 <anmega83@gmail.com>
2025-07-17 10:13:55 +0700

    add command line argument code, command line box when no argument

--- SVG-Reader/main.cpp
+++ SVG-Reader/main.cpp
@@ -1,6 +1,10 @@
 #include "stdafx.h"
 #include "SVGReader.h"
 
+#include <shellapi.h>
+
+#include <iostream>
+
 // add hPrevInstance and lpCmdLine (not use these)
 // _inopt_, _in_
 // int nCmdShow instead of INT iCmdShow
@@ -9,6 +13,50 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance,
 					_In_opt_ HINSTANCE hPrevInstance,
 					_In_ LPSTR lpCmdLine,
 					_In_ int nCmdShow) {
+
+	// set the window parameters
+	SVGReader::getInstance().setWinParams(hInstance, nCmdShow);
+
+	LPWSTR* szArglist;
+
+	szArglist = CommandLineToArgvW(GetCommandLineW(), &nCmdShow);
+	if (NULL == szArglist)
+	{
+		wprintf(L"CommandLineToArgvW failed\n");
+		return 0;
+	}
+	
+	
+	if (nCmdShow != 2)
+	{
+		SVGReader::getInstance().showCmdNoArgumentBox();
+		return -1;
+	}
+
+	wstring ws(szArglist[1]);
+	string myVarS = string(ws.begin(), ws.end());
+
+	SVGReader::getInstance().init(myVarS);
+
+
+	// Free memory allocated for CommandLineToArgvW arguments.
+	LocalFree(szArglist);
+
+	
+
+	// init GDI+, WNDCLASSEX, HWND, and show the window with just the default file name
+	// (abstraction)
+
+	// the main loop for processing messages
+	SVGReader::getInstance().messageLoop();
+
+	// shutdown the gdi+
+	return SVGReader::getInstance().cleanup();
+
+
+
+
+
 
 	// Use Singleton design pattern as this program is designed to 
 	// display only 1 image, also it is simple to design. 
