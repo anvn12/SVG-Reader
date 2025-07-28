5554dafb6142d084b417d6bc37a177737b920b59
anvn12 <anmega83@gmail.com>
2025-07-28 22:10:59 +0700

    add command line handler in main.cpp

--- SVG-Reader/main.cpp
+++ SVG-Reader/main.cpp
@@ -6,67 +6,12 @@
 #include <iostream>
 
 // add hPrevInstance and lpCmdLine (not use these)
-// _inopt_, _in_
-// int nCmdShow instead of INT iCmdShow
+// _In_opt_, _In_ int nCmdShow instead of INT iCmdShow
 // int WINMAIN instead of INT to ignore the warning
 int WINAPI WinMain(_In_ HINSTANCE hInstance,
 					_In_opt_ HINSTANCE hPrevInstance,
 					_In_ LPSTR lpCmdLine,
 					_In_ int nCmdShow) {
-
-
-	// set hInstance and nCmdShow before command line proc
-	//because if the command line takes no arguments, it will display
-	//to the screen and these 2 variables direct the screen
-	// 
-	// set the window parameters
-	SVGReader::getInstance().setWinParams(hInstance, nCmdShow);
-
-
-	// ref: https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-commandlinetoargvw?redirectedfrom=MSDN
-	// command line proc
-	LPWSTR* szArglist;
-	int nArgs;
-
-	szArglist = CommandLineToArgvW(GetCommandLineW(), &nArgs);
-	if (NULL == szArglist)
-	{
-		wprintf(L"CommandLineToArgvW failed\n");
-		return 0;
-	}
-	
-	
-	if (nArgs != 2)
-	{
-		SVGReader::getInstance().showCmdNoArgumentBox();
-		return -1;
-	}
-
-	wstring ws(szArglist[1]);
-	string myVarS = string(ws.begin(), ws.end());
-
-	SVGReader::getInstance().init(myVarS);
-
-
-	// Free memory allocated for CommandLineToArgvW arguments.
-	LocalFree(szArglist);
-
-	
-
-	// init GDI+, WNDCLASSEX, HWND, and show the window with just the default file name
-	// (abstraction)
-
-	// the main loop for processing messages
-	SVGReader::getInstance().messageLoop();
-
-	// shutdown the gdi+
-	return SVGReader::getInstance().cleanup();
-
-
-
-
-
-
 	// Use Singleton design pattern as this program is designed to 
 	// display only 1 image, also it is simple to design. 
 	// 
@@ -74,13 +19,39 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance,
 	// 
 	// The concept is similar to a web browser tab (it only displays 1 at a time).
 
-
+	// set hInstance and nCmdShow before command line proc
+	//because if the command line takes no arguments, it will display
+	//to the screen by these 2 variables 
+	// 
 	// set the window parameters
 	SVGReader::getInstance().setWinParams(hInstance, nCmdShow);
 
-	// init GDI+, WNDCLASSEX, HWND, and show the window with just the default file name
-	// (abstraction)
-	SVGReader::getInstance().init("sample.svg");
+	// if launch by MSVS (not by command line), it shows the "sample.svg" file as default file name
+
+	// ref: https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-commandlinetoargvw?redirectedfrom=MSDN
+	// command line proc
+	LPWSTR* szArglist;
+	int nArgs;
+
+	szArglist = CommandLineToArgvW(GetCommandLineW(), &nArgs);
+	if (NULL == szArglist) {
+		wprintf(L"CommandLineToArgvW failed\n");
+		return 0;
+	}
+	
+	if (nArgs != 2)	{
+		SVGReader::getInstance().showCmdNoArgumentBox();
+		return -1;
+	}
+
+	wstring ws(szArglist[1]);	// buffer variable to convert wstring to string
+	string filename = string(ws.begin(), ws.end());
+
+	// Free memory allocated for CommandLineToArgvW arguments because the file name is passed to 
+	LocalFree(szArglist);
+
+	// init the program with file name given in command line
+	SVGReader::getInstance().init(filename);
 
 	// the main loop for processing messages
 	SVGReader::getInstance().messageLoop();
