2f39331cdb85db9736a81199af4696d16d898ab8
anvn12 <anmega83@gmail.com>
2025-08-04 20:17:28 +0700

    add transform order, add TransformAttribute, TransformTranslate, TransformScale, TransformRotate classes

--- SVG-Reader/Transform.h
+++ SVG-Reader/Transform.h
@@ -1,69 +1,122 @@
-
 #ifndef Transform_h
 #define Transform_h
 #pragma once
-
-// Dinh nghia pi
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
 
 #include <vector>
 #include <string>
 #include <gdiplus.h>
 using namespace std;
 
-enum TransformType {    //de dinh nghia cac hang so co ten. Kiêu nhu la khi goi TRANSLATE thì no tu hieu la tinh tien...
-    TRANSLATE,
-    ROTATE,
-    SCALE
-};
 
-//struct TransformOperation {
-//    TransformType type;
-//    float values[3]; // 3 gia tri (x, y, scale)
-//    int valueCount;
-//
-//    TransformOperation(TransformType t) : type(t), valueCount(0) {
-//        values[0] = values[1] = values[2] = 0.0f;
-//    }
-//};
+// declaration
+class TransformAttribute;
+class TransformTranslate;
+class TransformScale;
+class TransformRotate;
 
-//class Transform {
-//private:
-//    vector<TransformOperation> operations;
-//    void parseParameters(const string& paramStr, vector<float>& values);
-//
-//public:
-//    Transform() {}
-//
-//    void parseTransform(const string& transformStr);
-//    void applyTransform(float& x, float& y) const;
-//    bool hasTransform() const { return !operations.empty(); }
-//
-//    size_t getOperationCount() const { return operations.size(); }
-//};
 
 class Transform {
 private:
-    float translateX, translateY;
-    float scaleX, scaleY;
-    float rotateAngle;
+    //float translateX, translateY;
+    //float scaleX, scaleY;
+    //float rotateAngle;
+
+    std::vector<TransformAttribute*> transformAttributeList;
 
     void parseParameters(const string& paramStr, vector<float>& values);
 
 public:
     Transform()
-        : translateX(0.0f), translateY(0.0f),
-        scaleX(1.0f), scaleY(1.0f),
-        rotateAngle(0.0f)
+        //: translateX(0.0f), translateY(0.0f),
+        //scaleX(1.0f), scaleY(1.0f),
+        //rotateAngle(0.0f)
     {
+    }
+
+    ~Transform() {
+        if (transformAttributeList.size() > 0) {
+            for (size_t i = 0; i < transformAttributeList.size(); i++) {
+                delete transformAttributeList[i];
+            }
+        }
     }
 
     void parseTransform(const string& transformStr);
 
     void applyToGraphics(Graphics* g) const;
+};
 
+// base class to store translate, rotate, scale in transform svg
+class TransformAttribute {
+public:
+    TransformAttribute() {}
+    virtual ~TransformAttribute() {}
+
+    virtual void applyToGraphics(Graphics* g) = 0;
+};
+
+class TransformTranslate : public TransformAttribute {
+private:
+    float translateX, translateY;
+public:
+    TransformTranslate()
+        : translateX(0.0f), translateY(0.0f) 
+    {}
+    // constructor with x
+    TransformTranslate(float x)
+        : translateX(x), translateY(0.0f)
+    {}
+    // constructor with x y
+    TransformTranslate(float x, float y)
+        : translateX(x), translateY(y) 
+    {}
+
+    ~TransformTranslate() override {}
+
+    void applyToGraphics(Graphics* g) override;
+};
+
+class TransformScale : public TransformAttribute {
+private:
+    float scaleX, scaleY;
+public:
+    TransformScale()
+        : scaleX(1.0f), scaleY(1.0f)
+    {
+    }
+    // constructor with x
+    TransformScale(float x)
+        : scaleX(x), scaleY(x)
+    {
+    }
+    // constructor with x y
+    TransformScale(float x, float y)
+        : scaleX(x), scaleY(y)
+    {
+    }
+
+    ~TransformScale() override {}
+
+    void applyToGraphics(Graphics* g) override;
+};
+
+class TransformRotate : public TransformAttribute {
+private:
+    float rotateAngle;
+public:
+    TransformRotate()
+        : rotateAngle(0.0f)
+    {
+    }
+    // constructor with x
+    TransformRotate(float x)
+        : rotateAngle(x)
+    {
+    }
+
+    ~TransformRotate() override {}
+
+    void applyToGraphics(Graphics* g) override;
 };
 
 #endif
