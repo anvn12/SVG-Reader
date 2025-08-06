c301c63de0ee7ac3ca0a9b252ddcf309c284c558
anvn12 <anmega83@gmail.com>
2025-08-06 20:40:56 +0700

    change default file to sample.svg

--- SVG-Reader/main.cpp
+++ SVG-Reader/main.cpp
@@ -55,7 +55,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance,
 	//=======================
 
 
-	SVGReader::getInstance().init("D:\\SVG-Reader\\Testcase\\TestCases\\svg-02.svg");
+	SVGReader::getInstance().init("sample.svg");
 
 	// the main loop for processing messages
 	SVGReader::getInstance().messageLoop();
